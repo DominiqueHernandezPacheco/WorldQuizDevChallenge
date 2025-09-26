d1b2c119f9e32cdf2cfe8dc485df45b8694ffdf7
Christian Hernandez <domipachecodominique27@hotmail.com>
2025-09-26 10:49:47 -0600

    Subo proyecto React completo al repo WorldQuizDevChallenge

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import StartPage from "./pages/StartPage";
+import QuizPage from "./pages/QuizPage";
+import Congratulation from './components/Congratulation.jsx';
+import "./App.css";
+
+function App() {
+
+  return (
+   
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<QuizPage />} />
+        </Routes>
+    </BrowserRouter>
+ )
+}
+
+export default App
